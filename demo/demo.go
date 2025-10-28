fa8b82be2785926f18395b67454949758e18c9a4
3316935645@qq.com <renmy11@qq.com>
2025-10-28 17:34:13 +0800

    undone example

--- demo/demo.go
+++ demo/demo.go
@@ -1,7 +1,5 @@
 package demo
 
-import "fmt"
-
 
 var sink interface{}
 
@@ -88,10 +86,10 @@ func caller5() {
 }
 */
 //******************************以下是caller-callee逃逸案例
-func print(){
-	//fmt.Print("bug")
-	fmt.Printf("bug,%s","test")
-}
+// func print(){
+// 	//fmt.Print("bug")
+// 	fmt.Printf("bug,%s","test")
+// }
 
 // func callee(p *int) **int {//Rtn
 // 	return &p
@@ -520,3 +518,91 @@ Referenced by multi-threads
 */
 
 
+// Transformer transforms bytes.
+type Transformer interface {
+	// Transform writes to dst the transformed bytes read from src, and
+	// returns the number of dst bytes written and src bytes read. The
+	// atEOF argument tells whether src represents the last bytes of the
+	// input.
+	//
+	// Callers should always process the nDst bytes produced and account
+	// for the nSrc bytes consumed before considering the error err.
+	//
+	// A nil error means that all of the transformed bytes (whether freshly
+	// transformed from src or left over from previous Transform calls)
+	// were written to dst. A nil error can be returned regardless of
+	// whether atEOF is true. If err is nil then nSrc must equal len(src);
+	// the converse is not necessarily true.
+	//
+	// ErrShortDst means that dst was too short to receive all of the
+	// transformed bytes. ErrShortSrc means that src had insufficient data
+	// to complete the transformation. If both conditions apply, then
+	// either error may be returned. Other than the error conditions listed
+	// here, implementations are free to report other errors that arise.
+	Transform(dst, src []byte, atEOF bool) (nDst, nSrc int, err error)
+
+	// Reset resets the state and allows a Transformer to be reused.
+	Reset()
+}
+
+type SpanningTransformer interface {
+	Transformer
+
+	// Span returns a position in src such that transforming src[:n] results in
+	// identical output src[:n] for these bytes. It does not necessarily return
+	// the largest such n. The atEOF argument tells whether src represents the
+	// last bytes of the input.
+	//
+	// Callers should always account for the n bytes consumed before
+	// considering the error err.
+	//
+	// A nil error means that all input bytes are known to be identical to the
+	// output produced by the Transformer. A nil error can be returned
+	// regardless of whether atEOF is true. If err is nil, then n must
+	// equal len(src); the converse is not necessarily true.
+	//
+	// ErrEndOfSpan means that the Transformer output may differ from the
+	// input after n bytes. Note that n may be len(src), meaning that the output
+	// would contain additional bytes after otherwise identical output.
+	// ErrShortSrc means that src had insufficient data to determine whether the
+	// remaining bytes would change. Other than the error conditions listed
+	// here, implementations are free to report other errors that arise.
+	//
+	// Calling Span can modify the Transformer state as a side effect. In
+	// effect, it does the transformation just as calling Transform would, only
+	// without copying to a destination buffer and only up to a point it can
+	// determine the input and output bytes are the same. This is obviously more
+	// limited than calling Transform, but can be more efficient in terms of
+	// copying and allocating buffers. Calls to Span and Transform may be
+	// interleaved.
+	Span(src []byte, atEOF bool) (n int, err error)
+}
+
+type cond struct {
+	tIn, tNotIn SpanningTransformer
+	f           func(rune) bool
+	check       func(rune) bool               // current check to perform
+	t           SpanningTransformer // current transformer to use
+}
+
+func test(){
+	a := &cond{
+		tIn:    nil,
+		tNotIn: nil,
+		f:      nil,
+	}
+	a.Reset()
+	//return Transformer{a}
+}
+func (t *cond) is(r rune) bool {
+
+	return false
+}
+
+
+// Reset implements transform.Transformer.
+func (t *cond) Reset() {
+	t.check = t.is
+	t.t = t.tIn
+	// t.t.Reset() // notIn will be reset on first usage.
+}
